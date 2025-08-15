a8b318cad285711431461d19218a4f6a86b4e882
alexy-os <agorbunoff.ru@gmail.com>
2025-08-15 17:19:15 +0300

    + @ui8kit/core dependency and refactoring of an application with the implementation of a dark mode switch. Also connected is the Nunito Sans font.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import { Block, Stack, Text, Button, Icon } from "@ui8kit/core"
+import { Moon, Sun } from "lucide-react"
+import { useCallback, useEffect, useState } from "react";
+
 function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Dark mode toggle
+  const toggleDarkMode = useCallback(() => {
+    setIsDarkMode(prev => {
+      const newMode = !prev;
+      document.documentElement.classList.toggle('dark', newMode);
+      return newMode;
+    });
+  }, []);
+
+  // Initialize dark mode from system preference
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const initialMode = mediaQuery.matches;
+    setIsDarkMode(initialMode);
+    document.documentElement.classList.toggle('dark', initialMode);
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-secondary/10 gap-4">
-      <h1 className="text-3xl font-bold">Hello World!</h1>
-      <p className="text-lg text-secondary-foreground">This is a minimal Vite SWC + Tailwind 3 + Shadcn colors React starter app</p>
-      <button className="bg-primary text-primary-foreground py-2 px-4 rounded-lg">Primary Button</button>
-    </div>
+    <Block h="screen" className="content-center bg-gradient-to-br from-secondary/15 to-primary/15">
+      <Stack ta="center" justify="center" align="center" gap="lg" mx="auto">
+        <Text size="3xl" fw="bold">Hello World!</Text>
+        <Text size="lg" c="secondary-foreground">This is a minimal Vite SWC + Tailwind 3 + Shadcn colors React starter app</Text>
+        <Button onClick={toggleDarkMode}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+          <Icon c="primary-foreground" lucideIcon={isDarkMode ? Moon : Sun} />
+        </Button>
+      </Stack>
+    </Block>
   )
 }
 
